e62ac72732a7a9e7416e5413a79f4575d34940c8
Alex Felipe <alexfelipevieira@gmail.com>
2018-12-28 10:36:06 -0200

    5.4

--- app/src/main/java/br/com/alura/agenda/ui/ListaAlunosView.java
+++ app/src/main/java/br/com/alura/agenda/ui/ListaAlunosView.java
@@ -1,18 +1,30 @@
 package br.com.alura.agenda.ui;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import br.com.alura.agenda.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
 import br.com.alura.agenda.ui.adapter.ListaAlunosAdapter;
 
 public class ListaAlunosView {
 
+    private final ListaAlunosAdapter adapter;
+    private final AlunoDAO dao;
+    private final Context context;
+
+    public ListaAlunosView(Context context) {
+        this.context = context;
+        this.adapter = new ListaAlunosAdapter(this.context);
+        this.dao = new AlunoDAO();
+    }
+
     public void confirmaRemocao(final MenuItem item) {
         new AlertDialog
-                .Builder(this)
+                .Builder(context)
                 .setTitle("Removendo aluno")
                 .setMessage("Tem certeza que quer remover o aluno?")
                 .setPositiveButton("Sim", (dialogInterface, i) -> {
@@ -29,13 +41,12 @@ public class ListaAlunosView {
         adapter.atualiza(dao.todos());
     }
 
-    public void remove(Aluno aluno) {
+    private void remove(Aluno aluno) {
         dao.remove(aluno);
         adapter.remove(aluno);
     }
 
     public void configuraAdapter(ListView listaDeAlunos) {
-        adapter = new ListaAlunosAdapter(this);
         listaDeAlunos.setAdapter(adapter);
     }
 }
