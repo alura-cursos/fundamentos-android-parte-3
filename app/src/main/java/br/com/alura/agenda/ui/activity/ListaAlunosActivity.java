e4945c78ba2406066be5ab18c31ebba3f3dda683
Alex Felipe <alexfelipevieira@gmail.com>
2021-01-13 11:20:55 -0300

    atualizando aula

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -5,14 +5,17 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import br.com.alura.agenda.R;
 import br.com.alura.agenda.dao.AlunoDAO;
